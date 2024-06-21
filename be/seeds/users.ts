3b4699d70bd610ecf6c78942e725f7b3cd7bc52f
CoreX20 <korneliussetiawan13@gmail.com>
2024-06-21 13:42:30 +0700

    update seed cars

--- be/seeds/users.ts
+++ be/seeds/users.ts
@@ -9,7 +9,7 @@ export async function seed(knex: Knex): Promise<void> {
     // Inserts seed entries
     await knex("users").insert([
         { id: 1, name: "superadmin", email: "superadmin@mail.com", encryptedPassword: password, role:"superadmin" },
-        // { id: 2, name: "admin", email: "admin@mail.com", encryptedPassword: password, role:"admin" },
-        // { id: 3, name : "member", email: "member@mail.com", encryptedPassword: password, role:"member" }
+        { id: 2, name: "admin", email: "admin@mail.com", encryptedPassword: password, role:"admin" },
+        { id: 3, name : "member", email: "member@mail.com", encryptedPassword: password, role:"member" }
     ]);
 };
